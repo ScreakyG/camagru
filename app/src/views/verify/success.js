e5cb92b018355a02a5244918bdab4bad86e66276
Ykaercs <francois.gnz@gmail.com>
2025-08-19 18:47:02 +0200

    better organisation for modal show

--- app/src/views/verify/success.js
+++ app/src/views/verify/success.js
@@ -1,75 +1,62 @@
 import { redirectTo } from "../../navigation.js";
 
-function showSuccess() {
-    const newDiv = document.createElement("div");
-
-    newDiv.innerHTML = /*html*/
-        `<dialog id="acc-validation-success-modal" class="modal">
-            <div class="modal-box">
-                <h2 class="text-center text-2xl font-bold">Verification successful</h2>
-                <div class="m-4 flex flex-col items-center">
-                    <p class="m-2 text-center">Your account is now verified, you may now proceed to log in</p>
-                    <button type="button" class="btn btn-primary mt-4">Login</button>
-                </div>
+// Fonctions qui changent le contenu de la modal.
+function showSuccess(modal) {
+    modal.innerHTML = /*html*/
+        `<div class="modal-box">
+            <h2 class="text-center text-2xl font-bold">Verification successful</h2>
+            <div class="m-4 flex flex-col items-center">
+                <p class="m-2 text-center">Your account is now verified, you may now proceed to log in</p>
+                <button type="button" class="btn btn-primary mt-4">Login</button>
             </div>
-        </dialog>`
-
-    const body = document.querySelector("body");
-    body.appendChild(newDiv);
-
-    const modal = document.getElementById("acc-validation-success-modal");
-    modal.showModal();
+        </div>`
 
     const loginBtn = modal.querySelector("button");
     loginBtn?.addEventListener("click", () => {
         modal.close();
         redirectTo("/login");
     })
-
-    // Supprime la <div> au dessus pour destroy la modal
-    modal.addEventListener("close", () => {
-        modal.parentElement.remove();
-    })
 }
 
-function showFailed() {
-    const newDiv = document.createElement("div");
+function showFailed(modal) {
+    modal.innerHTML = /*html*/
+        `<div class="modal-box">
+            <h2 class="text-center text-2xl font-bold">Verification failed</h2>
+        </div>`
+}
 
+export function showVerifyStatus() {
+    // On recuperer les query parameters de la requete.
+    const urlParams = new URLSearchParams(window.location.search);
+    const status = urlParams.get("status");
+
+    const newDiv = document.createElement("div");
     newDiv.innerHTML = /*html*/
-        `<dialog id="acc-validation-failed-modal" class="modal">
-            <div class="modal-box">
-                <h2 class="text-center text-2xl font-bold">Verification failed</h2>
-            </div>
+        `<dialog id="verification-modal" class="modal">
         </dialog>`
+
+    const modal = newDiv.querySelector("dialog");
+
+    switch (status) {
+        case "success":
+            showSuccess(modal);
+            break;
+
+        case "failed":
+            showFailed(modal);
+            break;
+
+        default:
+            break;
+    }
 
     const body = document.querySelector("body");
     body.appendChild(newDiv);
 
-    const modal = document.getElementById("acc-validation-failed-modal");
     modal.showModal();
 
     // Supprime la <div> au dessus pour destroy la modal
     modal.addEventListener("close", () => {
         modal.parentElement.remove();
     })
-}
-
-export function showVerifyStatus() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const status = urlParams.get("status");
-    console.log(status);
-
-
-    switch (status) {
-        case "success":
-            showSuccess();
-            break;
-
-        case "failed":
-            showFailed();
-            break;
-
-        default:
-            break;
-    }
 }
