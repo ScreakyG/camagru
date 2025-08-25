d8512b549b37371428d61d025dd5c08c3d8f2a44
Ykaercs <francois.gnz@gmail.com>
2025-08-25 21:08:49 +0200

    /api/auth/forgot-password is now neutral in the reply if the email provided is linked to a account or not

--- back/src/routes/auth/forgot-password.js
+++ back/src/routes/auth/forgot-password.js
@@ -17,20 +17,19 @@ export async function forgotPassword(request, reply) {
             throw new Error("No email in body");
 
         const user = await findUserByEmail(email);
-        if (!user || !user.isVerified)
-            throw new Error("No account with this email / not verified");
+        if (user && user.isVerified)
+        {
+            const token = createValidationToken();
+            const hash = hashToken(token);
 
-        const token = createValidationToken();
-        const hash = hashToken(token);
-
-        await storeTokenDatabase(user, "reset_pw_token_hash", hash);
-        await sendPasswordResetMail(user, token);
-
-        return reply.send({success: true, message: "Reset password mail sent"});
+            await storeTokenDatabase(user, "reset_pw_token_hash", hash);
+            await sendPasswordResetMail(user, token);
+        }
+        return reply.send({success: true, message: "If an account exist check your emails."});
     }
     catch (error)
     {
         // Je devrais egalement renvoyer un message positif pour ne pas dire si cela a reussi.
-        return reply.send({success: false, message: error.message});
+        return reply.code(500).send({success: false, message: error.message});
     }
 }
