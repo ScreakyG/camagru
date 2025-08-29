a5ea744ead1efe36e9225769d6ceae36391516dd
Ykaercs <francois.gnz@gmail.com>
2025-08-29 21:44:25 +0200

    already verified accounts can not use resend mail endpoint

--- back/src/routes/auth/resend-validation-email.js
+++ back/src/routes/auth/resend-validation-email.js
@@ -6,13 +6,6 @@ import { sendValidationMail } from "../../utils/mailService.js";
 export async function resendValidationLink(request, reply) {
     try
     {
-        /**
-         * STEPS:
-         * 1/ Check if email provided in request
-         * 2/ Find user associated with this email
-         * 3/ Replace old validation token associated with user and set new.
-         * 4/ Send a new mail with the new token
-         */
         const { email } = request.body;
         if (!email)
             throw new Error("No email in request");
@@ -20,6 +13,10 @@ export async function resendValidationLink(request, reply) {
         let user = await findUserByEmail(email);
         if (!user)
             throw new Error("No user found with this email");
+
+        if (user.isVerified)
+            throw new Error("Account is already verified");
+
 
         const token = createValidationToken();
         const hashedToken = hashToken(token);
