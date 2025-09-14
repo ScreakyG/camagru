6f0dddd98a91163f68960ba0fb3cc7326d288cf8
Ykaercs <francois.gnz@gmail.com>
2025-09-14 22:49:22 +0200

    frontend update password form is ok , need to do endpoint

--- app/src/views/settings.js
+++ app/src/views/settings.js
@@ -1,23 +1,66 @@
-import { getFormValues } from "../utils.js";
+import { getFormValues, printAPIResponse } from "../utils.js";
 
-function submitProfileForm(profileForm) {
+async function submitProfileForm(profileForm) {
     const formValues = getFormValues(profileForm);
     console.log(formValues);
-    
+
+    try
+    {
+        const response = await fetch("/api/user/modify-password", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formValues)
+        });
+        const resData = await response.json();
+        if (!response.ok)
+        {
+            printAPIResponse("/api/user/modify-password", resData);
+            return;
+        }
+        printAPIResponse("/api/user/modify-password", resData);
+        return;
+    }
+    catch (error)
+    {
+        console.error("Error while fetching API /api/user/modify-password");
+    }
+}
+
+function repeatPasswordMatch(updatePasswordForm) {
+    const form = getFormValues(updatePasswordForm);
+
+    if (form.password !== form.repeatPassword)
+        return (false);
+    return (true);
 }
 
 function handleSettingsForms() {
     const updateProfileForm = document.getElementById("update-profile-form");
     const updatePasswordForm = document.getElementById("update-password-form");
 
-    updateProfileForm?.addEventListener("submit", (event) => {
+    const repeatPasswordInput = document.querySelector("input[name=repeatPassword]");
+    repeatPasswordInput?.addEventListener("focus", () => {
+        repeatPasswordInput.classList.remove("border-error");
+        repeatPasswordInput.classList.remove("border-success");
+    })
+
+    updateProfileForm?.addEventListener("submit", async (event) => {
         event.preventDefault()
+        // Verifier les inputs pour le front;
         submitProfileForm(updateProfileForm);
     });
 
     updatePasswordForm?.addEventListener("submit", (event) => {
         event.preventDefault();
-        console.log("Password form called");
+        if (!repeatPasswordMatch(updatePasswordForm))
+        {
+            repeatPasswordInput.classList.add("border-error");
+            return;
+        }
+        console.log("Submiting password change form");
+        // Subbmit le form.
     });
 }
 
@@ -41,7 +84,7 @@ export function showSettingsView(currentUser) {
                 <form id="update-profile-form" name="update-profile-form">
                     <div class="flex flex-col">
                         <label class="mb-1">Username</label>
-                        <input type="text" name="username" class="input input-bordered text-white ml-1" placeholder=${user.username} value=${user.username}></input>
+                        <input type="text" name="username" class="input input-bordered text-white ml-1"placeholder=${user.username} value=${user.username}></input>
                     </div>
                     <div class="flex flex-col my-5">
                         <label class="mb-1">Email address</label>
@@ -58,11 +101,18 @@ export function showSettingsView(currentUser) {
                 <form id="update-password-form" name="update-password-form">
                     <div class="flex flex-col">
                         <label class="mb-1">New password</label>
-                        <input type="password" class="input input-bordered text-white ml-1" placeholder="*********" required></input>
+                        <input type="password" name="password" class="validator input input-bordered text-white ml-1" required required pattern="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,128}" minlength="8"></input>
+                        <div class="validator-hint hidden ml-1">
+                            Must be between 8 and 128 characters, including<br />
+                            At least one number <br />
+                            At least one lowercase letter <br />
+                            At least one uppercase letter <br />
+                            At least one special character !@#$%^&*
+                        </div>
                     </div>
                     <div class="flex flex-col my-5">
                         <label class="mb-1">Repeat new password</label>
-                        <input type="password" class="input text-white ml-1" required></input>
+                        <input type="password" name="repeatPassword" class="input text-white ml-1" required></input>
                     </div>
                     <button type="submit" class="btn btn-primary ml-1">Save</button>
                 </form>
