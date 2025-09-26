a83acdfd3619b2128d6c57bb1b192903dc2ed848
Ykaercs <francois.gnz@gmail.com>
2025-09-26 16:23:40 +0200

    perform username and email availability in modify-user-infos endpoint

--- back/src/controllers/user/modifyUserInfos.js
+++ back/src/controllers/user/modifyUserInfos.js
@@ -1,11 +1,16 @@
-import { findUserById } from "../../models/querys.js";
-import { AuthenticationError } from "../../utils/errors.js";
+import { findUserByEmail, findUserById, findUserByUsername } from "../../models/querys.js";
+import { AuthenticationError, ConflictError } from "../../utils/errors.js";
 import { verifyJWT } from "../../utils/jwt.js";
 import { verifyEmailInput, verifyUsernameInput } from "../../validators/validation_rules.js";
 
 export async function modifyUserInfos(request, reply) {
     try
     {
+        /**
+         * TODO :
+         *  - Update les changements dans la DB.
+         */
+
         const auth_token = request.cookies.auth_token;
         if (!auth_token)
             throw new AuthenticationError("Could not find auth_token in cookies.");
@@ -21,19 +26,35 @@ export async function modifyUserInfos(request, reply) {
         const newUsername = verifyUsernameInput(username);
         const newEmail = verifyEmailInput(email);
 
+        const user = await findUserById(decodedToken.id);
+        if (!user)
+           throw new AuthenticationError("Auth_token is invalid/expired.");
+
+        console.log("user : ", user);
         console.log("new username : " + newUsername);
         console.log("new email : " + newEmail);
 
-        const user = await findUserById(decodedToken.id);
-        console.log("user : ", user);
+        const emailTaken = await findUserByEmail(newEmail);
+        const usernameTaken = await findUserByUsername(newUsername);
 
-        /**
-         * TODO :
-         *  - Trancher pour savoir si je fais la verification si le username est deja pris en JS ou SQL.
-         *  - Update seulement si le username n'est pas le meme, verifier en JS ou SQL ?
-         */
+        if (newUsername !== user.username && newEmail !== user.username)
+        {
+            if (emailTaken && usernameTaken)
+                throw new ConflictError("Email & username are already taken.");
+        }
+        if (newEmail !== user.email)
+        {
+            if (emailTaken)
+                throw new ConflictError("Email is already taken.");
+        }
+        if (newUsername !== user.username)
+        {
+            if (usernameTaken)
+                throw new ConflictError("Username is already taken.");
+        }
 
-        return (reply.code(200).send({ success: true, message: 'All good' }));
+
+        return (reply.code(200).send({ success: true, message: 'User informations updated' }));
     }
     catch (error)
     {
