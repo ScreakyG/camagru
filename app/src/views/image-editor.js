83a437c84cbb9a506587bb778b782c4de077fb2d
Ykaercs <francois.gnz@gmail.com>
2025-10-02 15:31:27 +0200

    improving ui when uploading files

--- app/src/views/image-editor.js
+++ app/src/views/image-editor.js
@@ -46,15 +46,17 @@ function updateImageDisplay(inputElement) {
         for (const file of inputFiles)
         {
             const listItem = document.createElement("li");
-            listItem.className = "my-2 h-10 flex justify-between border";
+            listItem.className = "mt-2 p-4 flex justify-between items-center border rounded-md";
+
             const para = document.createElement("p");
 
             // Verifier le type du fichier (png, jpg..);
-            para.textContent = `File name ${file.name}, file size ${file.size}.`;
+            para.textContent = `File name ${file.name}, file size ${file.size} bytes.`;
 
             const image = document.createElement("img");
             image.src = URL.createObjectURL(file);
             image.alt = image.title = file.name;
+            image.className = "size-16"
 
             listItem.appendChild(para);
             listItem.appendChild(image);
@@ -74,13 +76,13 @@ export function showImageEditorView() {
     imageEditorDiv.innerHTML = /*html*/ `
         <div class="text-black flex flex-col gap-5 bg-zinc-200 min-h-dvh px-4 py-8">
             <h1 class="text-2xl text-center">Editor</h1>
-            <div class="h-50 p-6 border-zinc-400 rounded-xl border sm:max-w-5xl mx-auto w-full">
+            <div class="min-h-50 p-6 border-zinc-400 rounded-xl border sm:max-w-5xl mx-auto w-full">
                 <form class="flex flex-col gap-5">
                     <div>
                         <label for="image_upload_input" class="btn">Choose images to upload (PNG, JPG)</label>
                         <input class="opacity-0" type="file" id="image_upload_input" name="image_upload" accept="image/*" required></input>
                     </div>
-                    <div class="bg-red-200" id="upload_preview">
+                    <div class="" id="upload_preview">
                         <p>No files currently selected for upload.</p>
                     </div>
                     <button class="btn">Upload</button>
