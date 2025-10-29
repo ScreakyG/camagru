ab1f409309d149e960205459064b0f467ff733e1
Ykaercs <francois.gnz@gmail.com>
2025-10-29 18:09:26 +0100

    minimal server-side editor form verification

--- back/src/controllers/user/publishImage.js
+++ back/src/controllers/user/publishImage.js
@@ -1,6 +1,52 @@
+import { BadRequestError } from "../../utils/errors.js";
+
+const availableOverlays = [
+    "frost_frame",
+    "cat_selfie",
+    "pixel_glasses",
+    "smile_glasses"
+];
+
+const allowedInputFileFormat = [
+    "image/jpeg",
+    "image/png"
+];
+
+function parseOverlay(overlayRequested) {
+    console.log("Selected overlay = ", overlayRequested);
+
+    if (!overlayRequested)
+        return (null);
+    if (overlayRequested.fieldname !== "overlay" || !overlayRequested.value)
+        return (null);
+
+    for (let i = 0; i < availableOverlays.length; i++)
+    {
+        if (overlayRequested.value === availableOverlays[i])
+            return (overlayRequested.value)
+    }
+    return (null);
+}
+
+function isValidImageFile(inputFile) {
+    console.log("Uploaded file = ", inputFile);
+
+    if (!inputFile)
+        return (false);
+
+    for (let i = 0; i < allowedInputFileFormat.length; i++)
+    {
+        if (inputFile.mimetype === allowedInputFileFormat[i])
+            return (true);
+    }
+    return (false);
+}
+
 export async function publishImage(request, reply) {
     let uploadedFile = null;
-    let overlay = null;
+    let uploadedFileMetadata = null
+    let overlayRequested = null;
+    let overlayParsed = null;
 
     try
     {
@@ -9,21 +55,27 @@ export async function publishImage(request, reply) {
         {
             if (part.type === "file")
             {
+                uploadedFileMetadata = part;
                 uploadedFile = await part.toBuffer();
             }
             else if (part.type === "field")
-            {
-                overlay = part;
-            }
+                overlayRequested = part;
         }
+        
+        if (!isValidImageFile(uploadedFileMetadata))
+            throw new BadRequestError("Uploaded file is not a image.");
 
-        console.log("Uploaded file = ", uploadedFile);
-        console.log("Selected overlay = ", overlay, overlay.value);
+        overlayParsed = parseOverlay(overlayRequested);
+        if (!overlayParsed)
+            throw new BadRequestError("Requested filter is not valid.");
+
         return (reply.send({success: true, message: "Endpoint success call"}));
     }
     catch(error)
     {
         console.log("CATCHED ERROR = ", error);
+        if (error.statusCode)
+            return (reply.code(error.statusCode).send({success: false, errorMessage: error.message}))
         return (reply.send({success: false, message: "Error with uploaded file/selected overlay."}))
     }
 }
