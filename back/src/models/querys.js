8db0fcc61cdcfeff81f02b9ff95f9e8dfb2fb3d9
Ykaercs <francois.gnz@gmail.com>
2025-11-03 20:15:44 +0100

    feed images is now ordered from oldest to newest

--- back/src/models/querys.js
+++ back/src/models/querys.js
@@ -139,7 +139,7 @@ export async function getAllUserImages(user) {
     const db = await getDB();
 
     // const query = `SELECT images.* FROM users JOIN images ON users.id = images.user_id WHERE users.id = ?`;
-    const query = `SELECT * FROM images WHERE images.user_id = ?`;
+    const query = `SELECT * FROM images WHERE images.user_id = ? ORDER BY created_at DESC`;
     const result = await db.all(query, [user.id]);
 
     return (result);
